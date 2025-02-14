5b7b8afeb96185ed0f776f1df4740306c6638ca3
Ases da Literatura <geral.ases@gmail.com>
2025-02-14 12:45:26 -0300

    passos front completos

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { TokenInterceptor } from './app/token.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    
+    // Habilita interceptors vindos do DI
+    provideHttpClient(withInterceptorsFromDi()),
+
+    // Registra nosso TokenInterceptor
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ]
+});
